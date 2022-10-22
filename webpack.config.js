a72d0d0af31d9754a5d30605168730214c129b7f
Gabriel <62155954+GABE400@users.noreply.github.com>
2022-10-22 22:23:43 +0200

    react native

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(
+    {
+      ...env,
+      babel: {
+        dangerouslyAddModulePathsToTranspile: ['nativewind'],
+      },
+    },
+    argv
+  );
+
+  config.module.rules.push({
+    test: /\.css$/i,
+    use: ['postcss-loader'],
+  });
+
+  // If you want to add a new alias to the config.
+  config.resolve.alias['moduleA'] = 'moduleB';
+
+  // Maybe you want to turn off compression in dev mode.
+  if (config.mode === 'development') {
+    config.devServer.compress = false;
+  }
+
+  // Or prevent minimizing the bundle when you build.
+  if (config.mode === 'production') {
+    config.optimization.minimize = false;
+  }
+
+  // Customize the config before returning it.
+  return config;
+};
